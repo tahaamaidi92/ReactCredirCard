333be5528086bdaaba3f15bb634d56ff0c6d43bb
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-05-16 14:07:38 +0100

    Updates

---
+++ precache-manifest.16e63532bafa469da392b105cea03a8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cac25fac19ae5bec8330ce9acb1b4c4",
+    "url": "/ReactCredirCard/index.html"
+  },
+  {
+    "revision": "d909f49adcd795490b94",
+    "url": "/ReactCredirCard/static/css/main.1ab2f124.chunk.css"
+  },
+  {
+    "revision": "39bf0ff8e0ba5f54d744",
+    "url": "/ReactCredirCard/static/js/2.1a2d9fe0.chunk.js"
+  },
+  {
+    "revision": "d909f49adcd795490b94",
+    "url": "/ReactCredirCard/static/js/main.e12c0f93.chunk.js"
+  },
+  {
+    "revision": "2847dd40c42076f0e605",
+    "url": "/ReactCredirCard/static/js/runtime~main.8efd00f0.js"
+  },
+  {
+    "revision": "20c0f4062c99bfbf16c4519e3371bc16",
+    "url": "/ReactCredirCard/static/media/background.20c0f406.jpg"
+  },
+  {
+    "revision": "a8c8030be44fdafe49518bd5325953a4",
+    "url": "/ReactCredirCard/static/media/pus.a8c8030b.png"
+  },
+  {
+    "revision": "007dcb603e915afd331ec2dc01661f94",
+    "url": "/ReactCredirCard/static/media/visa.007dcb60.png"
+  }
+]);
